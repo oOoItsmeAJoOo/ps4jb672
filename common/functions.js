48a89b184c0fc02136cb75bfd91616c742c25cc3
oOoItsmeAJoOo <Testpage.oOoItsmeAJoOo@users.noreply.github.com>
2020-08-02 18:34:33 +0700

    update02082020
    update02082020

--- common/functions.js
+++ common/functions.js
@@ -1,17 +1,18 @@
 function jb_finished()
 {
     if(main_ret == 179 || main_ret == 0){
-		document.cookie = "isDone=Yes;";
+		setCookie("JB","DONE",30);
 		setTimeout(function(){document.getElementById("progress").innerHTML="PS4 Jailbreak 6.72 Exploit Complete ✔"; }, 500);
-	}else{
-        alert("Jailbreak failed! Reboot your PS4 and try again.");
-	}
+    } else{
+        	setTimeout(function(){document.getElementById("progress").innerHTML="Jailbreak failed! Reboot your PS4 and try again!!"; }, 500);
+    }
 }
 
 function payload_finished()
 {
+	setCookie("JB","DONE",30);
 	setTimeout(function(){document.getElementById("progress").innerHTML="Load Successful!!"; }, 3000);
-	setTimeout(function(){document.getElementById("progress").innerHTML="PS4 Jailbreak 6.72 Payload Loaded Succesfully ✔"; }, 5000);
+	setTimeout(function(){document.getElementById("progress").innerHTML="PS4 Jailbreak 6.72 Payload Loaded Succesfully ✔"; }, 7000);
 }
 
 function SC(x)
@@ -30,12 +31,73 @@ function PAYLOAD(x)
 }
 
 function load_JB()
-{	var isDone=getCookie("isDone");
-	if (isDone == "") {
-		if(confirm("Shall We Start with PS4 Jailbreak 6.72?\nNote: Click 'Cancel' if Jailbreak is already Complete after Starting your PS4!!")){			document.write(JB('jb')+'<script>jb_finished();</scr'+'ipt>');
-			document.write(JB('jb')+'<script>jb_finished();</scr'+'ipt>');	
-		}
+{	var jbDone=getCookie("JB");
+	if (jbDone == "" || jbDone == "START"){
+		setCookie("JB","START",30);
+		exploit();	
+	}else{
+		setTimeout(function(){document.getElementById("progress").innerHTML="PS4 Jailbreak 6.72 Exploit Complete ✔"; }, 500);
 	}
+}
+
+function exploit(){
+	if(document.getElementById('table') == null){
+		document.write('<style>'+
+						'.button {'+
+						'  background-color: #003263;'+
+						'  border-radius: 5px;'+
+						'  color: white;'+
+						'  padding: .5em;'+
+						'  text-decoration: none;'+
+						'  height:100%;'+
+						'  display:inline-table;'+
+						'}'+
+						'.button:focus,'+
+						'.button:hover {'+
+						'  background-color: #007bff;'+
+						'  color: White;'+
+						'}'+
+						'</style>');
+		document.write('<h1 id="progress" style="margin-top:30px;text-align:center;">Running Jailbreak Exploit!!</h1><hr>'+
+			       		'<div>'+
+						'<table id="table" align="center" style="width:600px;margin-top:30px;">'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'mirahen\'); return false" style="width:28%">MIRA + HEN</a>&nbsp;'+
+						'<a href="#" class="button" onclick="load_payload(\'miranohb\'); return false" style="width:28%">MIRA No HB</a>&nbsp;'+
+						'<a href="#" class="button" onclick="load_payload(\'miraunofficial\'); return false" style="width:28%">Mira UnOfficial</a></td>'+
+						'</tr>'+
+						'<tr><td><br/></td></tr>'+
+						'<tr>'+
+						'<td colspan="2" align="center"><h1>Payloads</h1></td>'+
+						'</tr>'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'app2usb\'); return false" style="width:43%">App2USB</a>&nbsp;'+
+						'<a href="#" class="button" onclick="load_netcat(); return false" style="width:43%">Bin Loader</a></td>'+
+						'</tr><tr><td><br/></td></tr>'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'disableupdates\'); return false" style="width:43%">Disable Updates</a>&nbsp;'+
+						'<a href="#" class="button" onclick="load_payload(\'enableupdates\'); return false" style="width:43%">Enable Updates</a></td>'+
+						'</tr><tr><td><br/></td></tr>'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'dumper\'); return false" style="width:43%">Dumper</a>&nbsp;'+
+						'<a href="#" class="button" onclick="load_payload(\'kerneldumper\'); return false" style="width:43%">Kernel Dumper</a></td>'+
+						'</tr><tr><td><br/></td></tr>'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'restore\'); return false" style="width:43%">Restore</a>&nbsp;'+
+						'<a href="#" class="button" onclick="load_payload(\'historyblocker\'); return false" style="width:43%">History Blocker</a></td>'+
+						'</tr><tr><td><br/></td></tr>'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'ftp\'); return false" style="width:43%">FTP</a>&nbsp;'+
+						'<a href="#" class="button" onclick="load_payload(\'backup\'); return false" style="width:43%">BackUp</a></td>'+
+						'</tr><tr><td><br/></td></tr>'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'linuxloader\'); return false" style="width:90%">Linux Loader</a></td>'+
+						'</tr>'+
+						'</table></div>');
+	}else{
+		document.write('<script>document.getElementById("progress").innerHTML="Running Jailbreak Exploit!!";</scr'+'ipt>');
+	}
+	setTimeout(function(){document.write(JB('set1')+JB('set2')+JB('set3')+JB('set4')+JB('jb')+'<script>jb_finished();</scr'+'ipt>');}, 500);
 }
 
 function load_netcat()
@@ -88,12 +150,16 @@ function load_netcat()
 						'<tr>'+
 						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'ftp\'); return false" style="width:43%">FTP</a>&nbsp;'+
 						'<a href="#" class="button" onclick="load_payload(\'backup\'); return false" style="width:43%">BackUp</a></td>'+
+						'</tr><tr><td><br/></td></tr>'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'linuxloader\'); return false" style="width:90%">Linux Loader</a></td>'+
 						'</tr>'+
 						'</table></div>');
 	}else{
 		document.write('<script>document.getElementById("progress").innerHTML="Loading! Please Wait!!";</scr'+'ipt>');
 	}
-	setTimeout(function(){document.write(PAYLOAD('mira')+PAYLOAD('c-code')+'<script>document.getElementById("progress").innerHTML="Awaiting Payload!! Send Payload To Port 9021";</scr'+'ipt>');}, 500);
+	setCookie("JB","START",30);
+	setTimeout(function(){document.write(PAYLOAD('mira')+PAYLOAD('c-code')+'<script>setCookie("JB","DONE",30);document.getElementById("progress").innerHTML="Awaiting Payload!! Send Payload To Port 9021";</scr'+'ipt>');}, 500);
 }
 
 function load_payload(payload)
@@ -146,16 +212,20 @@ function load_payload(payload)
 						'<tr>'+
 						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'ftp\'); return false" style="width:43%">FTP</a>&nbsp;'+
 						'<a href="#" class="button" onclick="load_payload(\'backup\'); return false" style="width:43%">BackUp</a></td>'+
+						'</tr><tr><td><br/></td></tr>'+
+						'<tr>'+
+						'<td align="center" colspan="2"><a href="#" class="button" onclick="load_payload(\'linuxloader\'); return false" style="width:90%">Linux Loader</a></td>'+
 						'</tr>'+
 						'</table></div>');
 	}else{
 		document.write('<script>document.getElementById("progress").innerHTML="Loading! Please Wait!!";</scr'+'ipt>');
 	}
+	setCookie("JB","START",30);
 	setTimeout(function(){document.write(PAYLOAD('mira')+PAYLOAD(payload)+PAYLOAD('c-code')+'<script>payload_finished();</scr'+'ipt>');}, 500);
 }
 
-function getCookie(cname) {
-  var name = cname + "=";
+function getCookie(name) {
+  var name = name + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
   for(var i = 0; i < ca.length; i++) {
@@ -168,4 +238,15 @@ function getCookie(cname) {
     }
   }
   return "";
+}
+
+function setCookie(name, value, mins) {
+  var d = new Date();
+  d.setTime(d.getTime() + (mins*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function deleteCookie(name) {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
